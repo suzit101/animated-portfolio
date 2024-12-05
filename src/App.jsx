407f778dc9ccef1bd9c68a8ebbc809fb8bbbee09
sujit101 <sujittamang5@gmail.com>
2024-12-05 21:07:02 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+        <Services />
+      </section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>Services</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <section>Portfolio</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section id="Contact">Contact</section>
+    </div>
+  );
 };
 
 export default App;
